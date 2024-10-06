d95bc97c04d26d10183d105b99ae60dc8a9c9724
mdnobin96670 <farhanahmednobin@gmail.com>
2024-10-06 16:24:38 +0600

    first commit

---
+++ content/featured/HalcyonTheme/index.md
@@ -0,0 +1,18 @@
+---
+date: '1'
+title: 'Blog Website'
+cover: './blog.png'
+github: 'https://github.com/md-nobin/Personal_Blog_Website'
+external: 'https://nobin-wave.onrender.com/'
+tech:
+  - Python
+  - Flask
+  - SQL-lite
+  - Bootstrap
+  - Heroku
+
+  # - iTerm2
+  # - Hyper
+---
+
+Minimal and clean Blog website. A feature-rich and secure blog website using the Flask framework.. Live on [Internet](https://nobin-wave.onrender.com/)
---
+++ src/components/loader.js
@@ -0,0 +1,125 @@
+import React, { useState, useEffect } from 'react';
+import { Helmet } from 'react-helmet';
+import PropTypes from 'prop-types';
+import anime from 'animejs';
+import styled from 'styled-components';
+import { IconLoader } from '@components/icons';
+
+const StyledLoader = styled.div`
+  ${({ theme }) => theme.mixins.flexCenter};
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  background-color: var(--dark-navy);
+  z-index: 99;
+
+  .logo-wrapper {
+    width: max-content;
+    max-width: 100px;
+    transition: var(--transition);
+    opacity: ${props => (props.isMounted ? 1 : 0)};
+    svg {
+      display: block;
+      width: 100%;
+      height: 100%;
+      margin: 0 auto;
+      fill: none;
+      user-select: none;
+      #B {
+        opacity: 0;
+      }
+    }
+  }
+`;
+
+const Loader = ({ finishLoading }) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  const animate = () => {
+    const loader = anime.timeline({
+      complete: () => finishLoading(),
+    });
+
+    loader
+      .add({
+        targets: '#logo path',
+        delay: 300,
+        duration: 1500,
+        easing: 'easeInOutQuart',
+        strokeDashoffset: [anime.setDashoffset, 0],
+      })
+      .add({
+        targets: '#logo #B',
+        duration: 700,
+        easing: 'easeInOutQuart',
+        opacity: 1,
+      })
+      .add({
+        targets: '#logo',
+        delay: 500,
+        duration: 300,
+        easing: 'easeInOutQuart',
+        opacity: 0,
+        scale: 0.1,
+      })
+      .add({
+        targets: '.loader',
+        duration: 200,
+        easing: 'easeInOutQuart',
+        opacity: 0,
+        zIndex: -1,
+      });
+  };
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setIsMounted(true), 10);
+    animate();
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return (
+    <StyledLoader className="loader" isMounted={isMounted}>
+      <Helmet bodyAttributes={{ class: `hidden` }} />
+
+      <div className="logo-wrapper">
+        <IconLoader />
+      </div>
+    </StyledLoader>
+  );
+};
+
+Loader.propTypes = {
+  finishLoading: PropTypes.func.isRequired,
+};
+
+export default Loader;
+// import React from 'react';
+
+// const IconLoader = () => (
+//   <svg id="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+//     <title>Loader Logo</title>
+//     <g>
+//       <g id="B" transform="translate(11.000000, 5.000000)"></g>
+//       <path
+//         stroke="currentColor"
+//         strokeWidth="5"
+//         strokeLinecap="round"
+//         strokeLinejoin="round"
+//         d="M 50, 5
+//                   L 11, 27
+//                   L 11, 72
+//                   L 50, 95
+//                   L 89, 73
+//                   L 89, 28 z"
+//       />
+//     </g>
+//     <text x="30" y="70" fill="currentColor" fontSize="55px" fontFamily="Consolas">
+//       N
+//     </text>
+//   </svg>
+// );
+// export default IconLoader;
